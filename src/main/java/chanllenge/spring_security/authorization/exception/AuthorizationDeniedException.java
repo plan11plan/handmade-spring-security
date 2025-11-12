1bb8f85c67917ace54e145dd2ac5b484a210557e
plan11plan <tjtfy7028@gmail.com>
2025-11-12 23:40:39 +0900

    refactor(AuthorizationDeniedException): 여러 생성자 추가

--- src/main/java/chanllenge/spring_security/authorization/exception/AuthorizationDeniedException.java
+++ src/main/java/chanllenge/spring_security/authorization/exception/AuthorizationDeniedException.java
@@ -1,12 +1,31 @@
 package chanllenge.spring_security.authorization.exception;
 
+import chanllenge.spring_security.authorization.model.AuthorizationResult;
+
+
 public class AuthorizationDeniedException extends RuntimeException {
 
+    private final AuthorizationResult authorizationResult;
+
     public AuthorizationDeniedException(String message) {
-        super(message);
+        this(message, (AuthorizationResult) null);
     }
 
     public AuthorizationDeniedException(String message, Throwable cause) {
+        this(message, null, cause);
+    }
+
+    public AuthorizationDeniedException(String message, AuthorizationResult authorizationResult) {
+        super(message);
+        this.authorizationResult = authorizationResult;
+    }
+
+    public AuthorizationDeniedException(String message, AuthorizationResult authorizationResult, Throwable cause) {
         super(message, cause);
+        this.authorizationResult = authorizationResult;
+    }
+
+    public AuthorizationResult getAuthorizationResult() {
+        return this.authorizationResult;
     }
 }
